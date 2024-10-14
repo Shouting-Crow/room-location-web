54e230b9fd9f833d9b3768fab810c438c0b8fa52
SHOUTING_CROW <jinwook5062@gmail.com>
2024-10-14 13:31:08 +0900

    [1] 프로젝트 생성

---
+++ src/main/java/com/project/roomlocationweb/RoomLocationWebApplication.java
@@ -0,0 +1,13 @@
+package com.project.roomlocationweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomLocationWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomLocationWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/roomlocationweb/RoomLocationWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.roomlocationweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomLocationWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
